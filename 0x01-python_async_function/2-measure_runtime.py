842092fc40a8c2cbd2876426a9a987e58ebaae3e
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2023-07-11 14:08:20 +0000

    Added task 2

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""
+measure_time
+"""
+import asyncio
+import time
+wait_n = __import__("1-concurrent_coroutines").wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    Measure time
+    Args:
+        n (int)
+        max_delay (int)
+    Return:
+        (float)
+    """
+    start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.time() / 2
+    return end / n
