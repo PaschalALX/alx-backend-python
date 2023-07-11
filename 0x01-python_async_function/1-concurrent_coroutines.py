2ca71db519016c3b41b94d28bda87736e228bfb0
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2023-07-11 12:49:45 +0000

    Refactored Task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,10 +3,11 @@
 """
 asynchronous coroutine that takes in an integer argument
 """
+from typing import List
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Wait for a random delay between 0 and max_delay
     Args:
