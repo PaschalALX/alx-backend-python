de52f83ca28ff741e97d682e6c096e1b16f0f449
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2023-07-11 13:25:57 +0000

    Refactored Task 1 * 3

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -19,5 +19,4 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     delays = []
     for x in range(n):
         delays.append(await wait_random(max_delay))
-    delays.sort()
-    return delays
+    return sorted(delays)
